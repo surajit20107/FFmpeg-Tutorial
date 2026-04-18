346f25c1c461693b8fc892fd85c483e177b3ae2a
Surajit <surajitjana20107@gmail.com>
2026-04-18 20:31:21 +0000

    update scripts to get output in a single dir

--- 01_slow_trim.sh
+++ 01_slow_trim.sh
@@ -4,4 +4,4 @@
 # pros: trim exactly provided seconds in my case 10 seconds
 # cons: slow, takes a lot of time
 # -ss 10: start the video at 10 seconds
-ffmpeg -i input.mp4 -ss 10 slow_trim.mp4
+ffmpeg -i input.mp4 -ss 10 output/slow_trim.mp4

1cbcb3af70199c84fe823a57f5ad5ee0a1d70ce5
Surajit <surajitjana20107@gmail.com>
2026-04-19 06:54:51 +0000

    mp4 to webm

--- 02_conversion.sh
+++ 02_conversion.sh
@@ -2,3 +2,9 @@
 
 # mp4 to mkv
 ffmpeg -i input.mp4 -c copy output/output.mkv
+
+# mp4 to webm
+# IMPORTANT: -c copy can cause error here
+ffmpeg -i input.mp4 output/output.webm
+# ro
+ffmpeg -i input.mp4 -c:v libvpx-vp9 -c:a libopus output/output.webm

4f1fb8e20dd22dd66a8802c707b892f8f0c0f7aa
Surajit <surajitjana20107@gmail.com>
2026-04-19 07:07:04 +0000

    mp4 to mp3 conversion

--- 02_conversion.sh
+++ 02_conversion.sh
@@ -6,5 +6,10 @@ ffmpeg -i input.mp4 -c copy output/output.mkv
 # mp4 to webm
 # IMPORTANT: -c copy can cause error here
 ffmpeg -i input.mp4 output/output.webm
-# ro
+# or
 ffmpeg -i input.mp4 -c:v libvpx-vp9 -c:a libopus output/output.webm
+
+
+# mp4 to mp3 (extract audio)
+# IMPORTANT: -c copy can cause error here
+ffmpeg -i input.mp4 output/output.mp3
